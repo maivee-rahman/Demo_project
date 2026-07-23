8a6edf00db0af72c69969946de1c6e6f9e808395
maivee-rahman <mdmaiveerahman@gmail.com>
2026-07-23 20:06:49 +0600

    modified first project

--- Dproject.py
+++ Dproject.py
@@ -1,5 +1,27 @@
-def repo_details(name):
-    print("This is a demo repo file, made by: ",name)
-    print("He is a AI/ML engineering Student.Currently learning DL.\nThank You!")
+import random
 
-repo_details("Maivee")
+# Generate a random target number between 1 and 100
+secret_number = random.randint(1, 100)
+attempts = 0
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+while True:
+    guess_input = input("Enter your guess: ")
+    
+    # Make sure the user entered a valid number
+    if not guess_input.isdigit():
+        print("Please enter a valid whole number.")
+        continue
+    
+    guess = int(guess_input)
+    attempts += 1
+    
+    if guess < secret_number:
+        print("Too low! Try a higher number.")
+    elif guess > secret_number:
+        print("Too high! Try a lower number.")
+    else:
+        print(f"Correct! You guessed the number in {attempts} attempts.")
+        break
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # New Project
 
-This repo was created in the local system. Created by Maivee.
+This is one of my favourite classic python game.
+I used to write this number guessing game when i was on very early stage of learning python.
+I am uploading this to practice git_github as my first project.
